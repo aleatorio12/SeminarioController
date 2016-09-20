a7ec772cca362c5ca21019a50747daf747875113
vitomany <vitomany@bluebox>
2016-09-19 21:30:05 -0600

    Commit de pruebas

---
+++ src/java/controller/EnteController.java
@@ -0,0 +1,129 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controller;
+
+import entities.Ente;
+import entities.Proyecto;
+import java.io.Serializable;
+import java.util.List;
+import javax.annotation.PostConstruct;
+import javax.enterprise.context.SessionScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+import stateless.EnteFacade;
+
+/**
+ *
+ * @author Victor Matías <Carné: 4490-13-5931> <vitomany@yahoo.es>
+ */
+
+@Named
+@SessionScoped
+
+
+public class EnteController implements Serializable{
+    
+    //Campos de la tabla Ente
+    
+    private int idEnte;
+    private String nombre;
+    private String direccion;
+    private char tipo;
+    
+    private List<Ente> listEnte = null;
+    
+    @Inject
+    private EnteFacade enteFacade;
+    @PostConstruct
+    public void init (  ){
+        enteFacade.findAll();
+    }
+    
+    public String cmdCreate(){
+        return "crearente.xhtml";
+    }
+
+    public String registrarEnte(){
+        Ente ente = new Ente();
+        ente.setNombre(nombre);
+        ente.setDireccion(direccion);
+        ente.setTipo(tipo);
+        enteFacade.create(ente);
+        return "index.xhtml";
+    }
+
+    
+    /**
+     * @return the listEnte
+     */
+    public List<Ente> getListEnte() {
+        return listEnte;
+    }
+
+    /**
+     * @param listEnte the listEnte to set
+     */
+    public void setListEnte(List<Ente> listEnte) {
+        this.listEnte = listEnte;
+    }
+
+    /**
+     * @return the idEnte
+     */
+    public int getIdEnte() {
+        return idEnte;
+    }
+
+    /**
+     * @param idEnte the idEnte to set
+     */
+    public void setIdEnte(int idEnte) {
+        this.idEnte = idEnte;
+    }
+
+    /**
+     * @return the nombre
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * @param nombre the nombre to set
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    /**
+     * @return the direccion
+     */
+    public String getDireccion() {
+        return direccion;
+    }
+
+    /**
+     * @param direccion the direccion to set
+     */
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    /**
+     * @return the tipo
+     */
+    public char getTipo() {
+        return tipo;
+    }
+
+    /**
+     * @param tipo the tipo to set
+     */
+    public void setTipo(char tipo) {
+        this.tipo = tipo;
+    }
+    
+}
---
+++ src/java/controller/ProyectoController.java
@@ -0,0 +1,254 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controller;
+
+import entities.Ente;
+import entities.Proyecto;
+import java.io.Serializable;
+import java.util.Date;
+import java.util.List;
+import javax.annotation.PostConstruct;
+import javax.enterprise.context.SessionScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+import stateless.ProyectoFacade;
+
+/**
+ *
+ * @author Victor Matías <Carné: 4490-13-5931> <vitomany@yahoo.es>
+ */
+
+@Named
+@SessionScoped
+
+public class ProyectoController implements Serializable{
+    
+    private List<Proyecto> listaProyecto = null;
+    
+    private int nog;
+    private String nombre;
+    private double monto;
+    private String direccion;
+    private Date fechaInicio;
+    private Date fechaFin;
+    private int impactoSocial;
+    private char estado = 'I';
+    private Ente enteEjecutor;
+    private Ente enteSupervisor;
+    private Ente enteRector;
+    private int fase;
+    
+    
+    /**
+     * @return the listaProyecto
+     */
+    public List<Proyecto> getListaProyecto() {
+        return listaProyecto;
+    }
+
+    /**
+     * @param listaProyecto the listaProyecto to set
+     */
+    public void setListaProyecto(List<Proyecto> listaProyecto) {
+        this.listaProyecto = listaProyecto;
+    }
+
+    @Inject
+    private ProyectoFacade proyectoFacade;    
+    @PostConstruct
+    public void init() {
+        //listaProyecto = proyectoFacade.findAll();
+    }
+    
+    public String cmdCreate (){
+        return "crearproyecto.xhtml";
+    }
+    
+    public String insertarProyecto(){
+        
+        Proyecto proyecto = new Proyecto();
+        proyecto.setDireccion(direccion);
+        proyecto.setNog(nog);
+        proyecto.setNombre(nombre);
+        proyecto.setMonto(monto);
+        proyecto.setImpactoSocial(impactoSocial);
+        proyecto.setEstado(estado);
+        proyecto.setEnteEjecutor(enteEjecutor);
+        
+        proyectoFacade.create(proyecto);
+        
+        return "index.html";
+    }
+
+    /**
+     * @return the nog
+     */
+    public int getNog() {
+        return nog;
+    }
+
+    /**
+     * @param nog the nog to set
+     */
+    public void setNog(int nog) {
+        this.nog = nog;
+    }
+
+    /**
+     * @return the nombre
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * @param nombre the nombre to set
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    /**
+     * @return the monto
+     */
+    public double getMonto() {
+        return monto;
+    }
+
+    /**
+     * @param monto the monto to set
+     */
+    public void setMonto(double monto) {
+        this.monto = monto;
+    }
+
+    /**
+     * @return the direccion
+     */
+    public String getDireccion() {
+        return direccion;
+    }
+
+    /**
+     * @param direccion the direccion to set
+     */
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    /**
+     * @return the fechaInicio
+     */
+    public Date getFechaInicio() {
+        return fechaInicio;
+    }
+
+    /**
+     * @param fechaInicio the fechaInicio to set
+     */
+    public void setFechaInicio(Date fechaInicio) {
+        this.fechaInicio = fechaInicio;
+    }
+
+    /**
+     * @return the fechaFin
+     */
+    public Date getFechaFin() {
+        return fechaFin;
+    }
+
+    /**
+     * @param fechaFin the fechaFin to set
+     */
+    public void setFechaFin(Date fechaFin) {
+        this.fechaFin = fechaFin;
+    }
+
+    /**
+     * @return the impactoSocial
+     */
+    public int getImpactoSocial() {
+        return impactoSocial;
+    }
+
+    /**
+     * @param impactoSocial the impactoSocial to set
+     */
+    public void setImpactoSocial(int impactoSocial) {
+        this.impactoSocial = impactoSocial;
+    }
+
+    /**
+     * @return the estado
+     */
+    public char getEstado() {
+        return estado;
+    }
+
+    /**
+     * @param estado the estado to set
+     */
+    public void setEstado(char estado) {
+        this.estado = estado;
+    }
+
+    /**
+     * @return the enteEjecutor
+     */
+    public Ente getEnteEjecutor() {
+        return enteEjecutor;
+    }
+
+    /**
+     * @param enteEjecutor the enteEjecutor to set
+     */
+    public void setEnteEjecutor(Ente enteEjecutor) {
+        this.enteEjecutor = enteEjecutor;
+    }
+
+    /**
+     * @return the enteSupervisor
+     */
+    public Ente getEnteSupervisor() {
+        return enteSupervisor;
+    }
+
+    /**
+     * @param enteSupervisor the enteSupervisor to set
+     */
+    public void setEnteSupervisor(Ente enteSupervisor) {
+        this.enteSupervisor = enteSupervisor;
+    }
+
+    /**
+     * @return the enteRector
+     */
+    public Ente getEnteRector() {
+        return enteRector;
+    }
+
+    /**
+     * @param enteRector the enteRector to set
+     */
+    public void setEnteRector(Ente enteRector) {
+        this.enteRector = enteRector;
+    }
+
+    /**
+     * @return the fase
+     */
+    public int getFase() {
+        return fase;
+    }
+
+    /**
+     * @param fase the fase to set
+     */
+    public void setFase(int fase) {
+        this.fase = fase;
+    }
+    
+}
