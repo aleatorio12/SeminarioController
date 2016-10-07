ec75a86651395de07812354a876ff85c55a4b2a2
dany <dany@debian.Dany>
2016-10-07 01:43:59 -0500

    yo mero

--- src/java/controller/FaseController.java
+++ src/java/controller/FaseController.java
@@ -7,9 +7,13 @@ package controller;
 
 import entities.Fase;
 import java.io.Serializable;
+import java.util.Date;
 import java.util.List;
+import javax.annotation.PostConstruct;
 import javax.enterprise.context.SessionScoped;
+import javax.inject.Inject;
 import javax.inject.Named;
+import stateless.FaseFacade;
 
 /**
  *
@@ -20,18 +24,75 @@ import javax.inject.Named;
 public class FaseController implements Serializable {
 
     private static final long serialVersionUID = -8215235587134021444L;
+//DATOS FASE
     
-    private List<Fase> list = null;
-
-    public List<Fase> getList() {
-        return list;
+   private int idfase;
+   private String nombre;
+   private Date fechaInicio;
+   private Date fechaFinalizacion;
+   private Character estado;
+   
+   
+   private List<Fase> listFase= null;
+   
+   @Inject
+    private FaseFacade faseFacade;
+    @PostConstruct
+    public void init (  ){
+        faseFacade.findAll();
+    }
+    
+     public String cmdCreate(){
+        return "/faces/fase/fase.xhtml";
     }
 
-    public void setList(List<Fase> list) {
-        this.list = list;
+    public String registrarFase(){
+        Fase fase = new Fase();
+        fase.setNombre(nombre);
+        fase.setFechaInicio(fechaInicio);
+        fase.setFechaFinalizacion(fechaFinalizacion);
+        fase.setEstado(estado);
+        
+        faseFacade.create(fase);
+        return "index.xhtml";
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Date getFechaInicio() {
+        return fechaInicio;
+    }
+
+    public void setFechaInicio(Date fechaInicio) {
+        this.fechaInicio = fechaInicio;
+    }
+
+    public Date getFechaFinalizacion() {
+        return fechaFinalizacion;
+    }
+
+    public void setFechaFinalizacion(Date fechaFinalizacion) {
+        this.fechaFinalizacion = fechaFinalizacion;
+    }
+
+    public Character getEstado() {
+        return estado;
+    }
+
+    public void setEstado(Character estado) {
+        this.estado = estado;
     }
     
     
+
+   
+   
     
     
 }
