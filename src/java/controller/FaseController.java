bcc4f9886085ca035278585ea3a40a320533585f
freddy <freddy@debian>
2016-11-05 12:50:20 -0600

    Merge origin/master
    Conflicts:
    	web/WEB-INF/templates/header.xhtml

--- src/java/controller/FaseController.java
+++ src/java/controller/FaseController.java
@@ -17,24 +17,30 @@ import stateless.FaseFacade;
 
 /**
  *
- * @author Cesar Cifuentes 4490-13-9410
+ * @author dany
  */
 @Named
 @SessionScoped
-public class FaseController implements Serializable{
-    
+public class FaseController implements Serializable {
+
     private static final long serialVersionUID = -8215235587134021444L;
-    
-    
+    //DATOS FASE
 
+    private String nombre=null;
+    private Date fechaInicio=null;
+    private Date fechaFinalizacion=null;
+    private String estado=null;
+    
+    private Fase fase=null;
+    
+    private boolean popup = true;
+ 
+    public String cmdRegresar(){
+        return "#{request.contextPath}/faces/fase/fase.xhtml";
+    
+    }
+    
     private List<Fase> list = null;
-    private Integer idfase = null;
-    private String Nombre = null;
-    private Date fechainicio = null;
-    private Date fechafinalizacion = null;
-    private String Estado = null;
-    private Fase fase = null;
-
 
     public List<Fase> getList() {
         return list;
@@ -44,44 +50,36 @@ public class FaseController implements Serializable{
         this.list = list;
     }
 
-    public Integer getIdfase() {
-        return idfase;
-    }
-
-    public void setIdfase(Integer idfase) {
-        this.idfase = idfase;
-    }
-
     public String getNombre() {
-        return Nombre;
+        return nombre;
     }
 
-    public void setNombre(String Nombre) {
-        this.Nombre = Nombre;
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
     }
 
-    public Date getFechainicio() {
-        return fechainicio;
+    public Date getFechaInicio() {
+        return fechaInicio;
     }
 
-    public void setFechainicio(Date fechainicio) {
-        this.fechainicio = fechainicio;
+    public void setFechaInicio(Date fechaInicio) {
+        this.fechaInicio = fechaInicio;
     }
 
-    public Date getFechafinalizacion() {
-        return fechafinalizacion;
+    public Date getFechaFinalizacion() {
+        return fechaFinalizacion;
     }
 
-    public void setFechafinalizacion(Date fechafinalizacion) {
-        this.fechafinalizacion = fechafinalizacion;
+    public void setFechaFinalizacion(Date fechaFinalizacion) {
+        this.fechaFinalizacion = fechaFinalizacion;
     }
 
     public String getEstado() {
-        return Estado;
+        return estado;
     }
 
-    public void setEstado(String Estado) {
-        this.Estado = Estado;
+    public void setEstado(String estado) {
+        this.estado = estado;
     }
 
     public Fase getFase() {
@@ -91,69 +89,63 @@ public class FaseController implements Serializable{
     public void setFase(Fase fase) {
         this.fase = fase;
     }
-    
-    
 
-        
+    public boolean isPopup() {
+        return popup;
+    }
+
+    public void setPopup(boolean popup) {
+        this.popup = popup;
+    }
+
+
+    
+    
+    
     @Inject
-    private FaseFacade faseFacade;
-    
+    private FaseFacade FaseFacade;
+        
     @PostConstruct
-    public void init (){
-     list = faseFacade.findAll();
+    public void init(){
+        
+    list=FaseFacade.findAll();
+                
     }
     
-    public String cmdCrearFase(){
-    Fase fase = new Fase();
-    fase.setNombre(Nombre);
-    fase.setFechaInicio(fechainicio);
-    fase.setFechaFinalizacion(fechafinalizacion);
-    fase.setEstado(Estado);
-    faseFacade.create(fase);
-    list = faseFacade.findAll();
-    return "buscarfase.xhtml";
+    public String cmdCrear(){
+    return "./crearfase.xhtml";
     }
     
-    public String cmdRedirige(Fase fase){
-    this.fase = fase;
-    idfase = fase.getIdFase();
-    Nombre = fase.getNombre();
-    fechainicio = fase.getFechaInicio();
-    fechafinalizacion = fase.getFechaFinalizacion();
-    Estado = fase.getEstado();
-    return"formModificar.xhtml";
+     public String cmdModificar(Fase fase) {
+         this.fase=fase;
+         nombre=fase.getNombre();
+         fechaInicio=fase.getFechaInicio();
+         fechaFinalizacion=fase.getFechaFinalizacion();
+         estado=fase.getEstado();
+    return "edit.xhtml";
+    }
+     
+    
+    public String cmdRegistrar(){
+        Fase fase = new Fase ();
+        fase.setNombre(nombre);
+        fase.setFechaInicio(fechaInicio);
+        fase.setFechaFinalizacion(fechaFinalizacion);
+        fase.setEstado(estado);
+        FaseFacade.create(fase);
+                return "fase.xhtml";
+        
     }
     
-    public String cmdModicacionFase(){
-    Fase fase = new Fase();
-    fase.setIdFase(idfase);
-    fase.setNombre(Nombre);
-    fase.setFechaInicio(fechainicio);
-    fase.setFechaFinalizacion(fechafinalizacion);
-    fase.setEstado(Estado);
-    faseFacade.edit(fase);
-    list = faseFacade.findAll();
-        return "buscarfase.xhtml";
+     public String cmdUpdate(){
+        Fase fase = new Fase ();
+        fase.setNombre(nombre);
+        fase.setFechaInicio(fechaInicio);
+        fase.setFechaFinalizacion(fechaFinalizacion);
+        fase.setEstado(estado);
+        FaseFacade.edit(fase);
+                return "fase.xhtml";
+        
     }
-    
-     public String cmdRedirige2(Fase fase){
-    this.fase = fase;
-    idfase = fase.getIdFase();
-    Nombre = fase.getNombre();
-    fechainicio = fase.getFechaInicio();
-    fechafinalizacion = fase.getFechaFinalizacion();
-    Estado = fase.getEstado();
-    return"formEliminar.xhtml";
-    }
-    
-    public String cmdEliminarFase(){
-    Fase fase = new Fase();
-    fase.setNombre(Nombre);
-    fase.setFechaInicio(fechainicio);
-    fase.setFechaFinalizacion(fechafinalizacion);
-    fase.setEstado(Estado);
-    faseFacade.remove(fase);
-    list = faseFacade.findAll();
-        return "buscarfase.xhtml";
-    }
+
 }
